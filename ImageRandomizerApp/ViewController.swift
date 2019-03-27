22ad4c12b1427d55a2d7011ea083b62b91dd2c22
Maksym Savisko <stowyn@gmail.com>
2019-03-27 20:17:32 +0200

    chore: init project

---
+++ ImageRandomizerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageRandomizerApp
+//
+//  Created by Maksym Savisko on 3/27/19.
+//  Copyright © 2019 Maksym Savisko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
